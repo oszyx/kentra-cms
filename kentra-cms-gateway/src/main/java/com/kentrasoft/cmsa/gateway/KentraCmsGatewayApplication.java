a360dede94a05abf90432bfadd34497f2c40f339
oszyx <1281676727@qq.com>
2018-08-28 10:18:21 +0800

    20180828

---
+++ kentra-cms-gateway/src/main/java/com/kentrasoft/cmsa/gateway/KentraCmsGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.kentrasoft.cmsa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class KentraCmsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentraCmsGatewayApplication.class, args);
+	}
+}
---
+++ kentra-cms-registry/src/main/java/com/kentrasoft/cmsa/registry/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.kentrasoft.cmsa.registry.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * CSRF保护默认是开启的，可以禁用掉即可(否则服务注册失败)
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
