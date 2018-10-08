320fd54f5b5979890eb564561925dc724c809ac1
liyang4 <2583085624@qq.com>
2018-10-08 09:25:53 +0800

    删除贵阳所有模块

--- kentra-cms-api/kentra-cms-gy-customer/src/main/java/com/kentrasoft/cms/service/CardPortDataService.java
+++
@@ -1,7 +0,0 @@
-package com.kentrasoft.cms.service;
-
-import com.kentrasoft.utils.plugin.BaseResult;
-
-public interface CardPortDataService {
-    BaseResult findByDrivingRecord(String drivingRecord);
-}
